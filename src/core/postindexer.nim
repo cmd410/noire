5f40a8803ee652840354b3289feb173d6e35af86
Crystal Melting Dot <stresspassing@gmail.com>
2022-06-18 14:50:15 +0300

    Fix sigsegv on code blocks with no attrs

--- src/core/postindexer.nim
+++ src/core/postindexer.nim
@@ -141,6 +141,8 @@ proc normalizeHtml(node: var XmlNode, ctxDir: string = "") =
     of "pre":
       # perform syntax highlighting
       for code in element.mitems:
+        if code.attrs == nil:
+          code.attrs = newStringTable(modeCaseInsensitive)
         if code.kind != xnElement: continue
         if code.tag != "code": continue
         let lang = block:
